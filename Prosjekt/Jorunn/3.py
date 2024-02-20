9f18e4d3657a30775304f30eb65309528c19392b
Jorunn <jorunn336@gmail.com>
2024-02-20 14:13:11 +0100

    oppdaterte
    gjorde det til en funksjon

--- Prosjekt/Jorunn/3.py
+++ Prosjekt/Jorunn/3.py
@@ -2,13 +2,19 @@
 
 import matplotlib.pyplot as plt
 
-år = [2015,2016,2017,2018,2019,2020,2021,2022,2023,2024]
-innbyggertall = [3,3,3,4,4,3,3,4,5,5]
+def norge():
+    år = [2015,2016,2017,2018,2019,2020,2021,2022,2023,2024]
+    innbyggertall = [3,3,3,4,4,3,3,4,5,5]
 
-plt.figure(facecolor='lavender')
-ax = plt.axes()
+    plt.figure(facecolor='lavender')
+    ax = plt.axes()
 
-ax.set_facecolor("lavender")
+    ax.set_facecolor("lavender")
 
-plt.plot(år, innbyggertall, color="purple")
-plt.show()
+    plt.plot(år, innbyggertall, color="purple", label = "Norge")
+
+    plt.xlabel("årstall")
+    plt.ylabel("innbyggertall")
+
+    plt.show()
+
